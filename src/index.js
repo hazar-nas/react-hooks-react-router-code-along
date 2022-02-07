60d0e7745c785d4af8b4d663ef2ce33c2fce438a
hazar-nas <hazarcan.nas@gmail.com>
2022-02-07 12:09:31 +0300

    revised

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById("root")
+); 
